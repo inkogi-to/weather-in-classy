b4fd43043f6e84c7b3b07fb026a0564d87bcc16a
Nazar Kobernyk <kogito.nk@gmail.com>
2024-07-17 17:52:57 +0100

    Enhanced  and  to handle location changes and fetching weather data.

--- src/App.jsx
+++ src/App.jsx
@@ -30,19 +30,15 @@ function formatDay(dateStr) {
   }).format(new Date(dateStr));
 }
 class App extends Component {
-  constructor(props) {
-    super(props);
+  state = {
+    location: "lisbon",
+    isLoading: false,
+    displayLocation: "",
+    weather: {},
+  };
 
-    this.state = {
-      location: "lisbon",
-      isLoading: false,
-      displayLocation: "",
-      weather: {},
-    };
-    this.fetchWeather = this.fetchWeather.bind(this);
-  }
-
-  async fetchWeather() {
+  fetchWeather = async () => {
+    if (this.state.location.length < 2) return this.setState({ weather: {} });
     try {
       this.setState({ isLoading: true });
       // 1) Getting location (geocoding)
@@ -71,21 +67,30 @@ class App extends Component {
     } finally {
       this.setState({ isLoading: false });
     }
+  };
+  setLocation = (e) => {
+    this.setState({ location: e.target.value });
+  };
+
+  componentDidMount() {
+    this.setState({ location: localStorage.getItem("location") || "" });
   }
 
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.location !== this.state.location) {
+      this.fetchWeather();
+
+      localStorage.setItem("location", this.state.location);
+    }
+  }
   render() {
     return (
       <div className="app">
         <h1>Classy Weather</h1>
-        <div>
-          <input
-            type="text"
-            placeholder="Search from location..."
-            value={this.state.location}
-            onChange={(e) => this.setState({ location: e.target.value })}
-          />
-        </div>
-        <button onClick={this.fetchWeather}>Get weather</button>
+        <Input
+          location={this.state.location}
+          onChangeLocation={this.setLocation}
+        />
         {this.state.isLoading && <p className="loader">Loading...</p>}
         {this.state.weather.weathercode && (
           <Weather
@@ -97,7 +102,24 @@ class App extends Component {
     );
   }
 }
+class Input extends Component {
+  render() {
+    return (
+      <div>
+        <input
+          type="text"
+          placeholder="Search from location..."
+          value={this.props.location}
+          onChange={this.props.onChangeLocation}
+        />
+      </div>
+    );
+  }
+}
 class Weather extends Component {
+  componentWillUnmount() {
+    console.log("component will unmount");
+  }
   render() {
     const {
       temperature_2m_max: max,
@@ -140,66 +162,3 @@ class Day extends Component {
 }
 
 export default App;
-// function App() {
-//   function getWeatherIcon(wmoCode) {
-//     const icons = new Map([
-//       [[0], "☀️"],
-//       [[1], "🌤"],
-//       [[2], "⛅️"],
-//       [[3], "☁️"],
-//       [[45, 48], "🌫"],
-//       [[51, 56, 61, 66, 80], "🌦"],
-//       [[53, 55, 63, 65, 57, 67, 81, 82], "🌧"],
-//       [[71, 73, 75, 77, 85, 86], "🌨"],
-//       [[95], "🌩"],
-//       [[96, 99], "⛈"],
-//     ]);
-//     const arr = [...icons.keys()].find((key) => key.includes(wmoCode));
-//     if (!arr) return "NOT FOUND";
-//     return icons.get(arr);
-//   }
-
-//   function convertToFlag(countryCode) {
-//     const codePoints = countryCode
-//       .toUpperCase()
-//       .split("")
-//       .map((char) => 127397 + char.charCodeAt());
-//     return String.fromCodePoint(...codePoints);
-//   }
-
-//   function formatDay(dateStr) {
-//     return new Intl.DateTimeFormat("en", {
-//       weekday: "short",
-//     }).format(new Date(dateStr));
-//   }
-
-//   async function getWeather(location) {
-//     try {
-//       // 1) Getting location (geocoding)
-//       const geoRes = await fetch(
-//         `https://geocoding-api.open-meteo.com/v1/search?name=${location}`
-//       );
-//       const geoData = await geoRes.json();
-//       console.log(geoData);
-
-//       if (!geoData.results) throw new Error("Location not found");
-
-//       const { latitude, longitude, timezone, name, country_code } =
-//         geoData.results.at(0);
-//       console.log(`${name} ${convertToFlag(country_code)}`);
-
-//       // 2) Getting actual weather
-//       const weatherRes = await fetch(
-//         `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&timezone=${timezone}&daily=weathercode,temperature_2m_max,temperature_2m_min`
-//       );
-//       const weatherData = await weatherRes.json();
-//       console.log(weatherData.daily);
-//     } catch (err) {
-//       console.err(err);
-//     }
-//   }
-
-//   return <div className="App">App</div>;
-// }
-
-// export default App;
